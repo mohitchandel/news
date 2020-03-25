a1f10e79a3838398df459990ecc6dd14dd04e80c
Mohit Chandel <mohitchandel639@gmail.com>
2020-03-25 21:57:06 +0530

    final

--- news/settings.py
+++ news/settings.py
@@ -116,6 +116,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
